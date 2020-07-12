6548b365e8580f4ce08e5be897d0f731cda817b0
Mayank-S1 <mayanksengar198@gmail.com>
2020-07-12 15:32:15 +0530

    intial commit

---
+++ Portfolio/blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Blog(models.Model):
+      title = models.CharField(max_length=225)
+      pub_date = models.DateTimeField()
+      image = models.ImageField(upload_to='images/')
+      body = models.TextField()
+
+def __str__(self):
+    return self.title
+
+def summary(self):
+    return self.body[:100]
+
